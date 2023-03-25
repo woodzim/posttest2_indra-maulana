94c246c99f16657c27694b1cbec3b1afacecb371
woodzim <104117101+woodzim@users.noreply.github.com>
2023-03-25 20:48:56 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,33 +1,4 @@
 
-// const tombol = document.getElementById("Submit")
-// tombol.onclick = login;
-// function login(e){
-//   e.preventDefault();
-//   const email = document.getElementById("em").value;
-//   const password = document.getElementById("pwd").value;
-
-//   	if (email == "" || password == "") {
-// 		alert("email dan password harus diisi");
-// 		return false;
-// 	}
-
-//   // ngambil nilai array dari localstorage
-//   const akun = JSON.parse(localStorage.getItem("akun"));
-
-//   //Periksa apakah array akun ada dan tidak kosong
-//   if (akun && akun.length) {
-//     // Melakukan perulangan pada array 
-//     for (let i = 0; i < akun.length; i++) {
-//       if (email == akun[i].email && password == akun[i].password) {
-//         window.location.href="beranda.html";
-//         alert("berhasil");
-//         return; 
-//       }else {
-// 		alert("email atau password salah");
-// 		return false;
-// 			}
-//     }
-//   }}
   
   // LOGIN
 const login = document.getElementById("Submit");
